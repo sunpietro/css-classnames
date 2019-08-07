4ecc8e6d9918ed02a2c5c3037f74006d42c754a6
Piotr Nalepa <piotr.nalepa@auto1.com>
2019-08-07 11:25:52 +0200

    Updated readme

--- README.md
+++ README.md
@@ -3,12 +3,26 @@ A simple helper to generate CSS class names string based on provided conditions
 
 ## Usage sample
 
+First of all you need to install the package in your project by running:
+
+`npm install @sunpietro/css-classnames`
+
+or 
+
+`yarn install @sunpietro/css-classnames`
+
+or by downloading files from this repository and including the file `css-classnames.min.js` in your HTML code using `script` tag:
+
+`<script async src="./js/css-classnames.min.js"></script>`
+
+Then if you installed it using either NPM or Yarn, you can start using it the following way:
+
 ```javascript
-import * as cssClassnames from 'css-classnames';
+import * as classnames from '@sunpietro/css-classnames';
 
 const isFluid = true;
 const items = [1, 2, 3];
-const classNameString = cssClassnames({
+const classNameString = classnames({
     'container': true,
     'container--fluid': isFluid,
     'container--filled': items.length > 1,
@@ -18,3 +32,23 @@ const classNameString = cssClassnames({
 console.log(classNamesString);
 // The output: 'container container--fluid container--filled'
 ```
+
+Otherwise, the script above will slightly change to:
+
+```javascript
+const isFluid = true;
+const items = [1, 2, 3];
+const classNameString = window.classnames({
+    'container': true,
+    'container--fluid': isFluid,
+    'container--filled': items.length > 1,
+    'container--empty': false
+});
+
+console.log(classNamesString);
+// The output: 'container container--fluid container--filled'
+```
+
+## Feedback and issues
+
+If you have any feedback, please create an issue in this repository
--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-
-
-(function (root, factory) {
-    if (typeof define === "function" && define.amd) {
-        define([], factory);
-    } else if (typeof exports === "object") {
-        module.exports = factory();
-    } else {
-        root.foo = factory();
+/*@preserve
+ * @sunpietro/css-classnames - CSS classnames helper tool v1.0.1
+ * https://github.com/sunpietro/css-classnames
+ *
+ * Copyright 2019 Piotr Nalepa
+ * https://blog.piotrnalepa.pl
+ *
+ * Released under the MIT license
+ * https://github.com/sunpietro/css-classnames/blob/master/LICENSE
+ *
+ * Date: 2019-08-06T21:30Z
+ */
+(function(root, factory) {
+  if (typeof define === "function" && define.amd) {
+    define([], factory);
+  } else if (typeof exports === "object") {
+    module.exports = factory();
+  } else {
+    root.foo = factory();
+  }
+})(typeof this == "undefined" ? window : this, function() {
+  const cssClassnames = classes => {
+    if (Object.prototype.toString.call(classes) !== "[object Object]") {
+      return "";
     }
-}(typeof this == 'undefined' ? window : this, function () {
-    const cssClassnames = (classes) => {
-      if (Object.prototype.toString.call(classes) !== '[object Object]') {
-          return '';
-      }
 
-      return Object.entries(classes)
-          .reduce((total, [name, condition]) => {
-              return `${total} ${condition ? name : ''}`;
-          }, '')
-          .trim();
-    };
-    
-    return cssClassnames;
-}));
+    return Object.entries(classes)
+      .reduce((total, [name, condition]) => {
+        return `${total} ${condition ? name : ""}`;
+      }, "")
+      .trim();
+  };
+
+  return cssClassnames;
+});
